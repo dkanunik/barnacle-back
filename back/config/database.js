55223e8474d86bc956b5bfb934cff2aa5785ee99
dkanunik <dmytro.kanunik@teamaol.com>
2019-01-17 19:35:33 +0200

    Perform CI workflow

--- back/config/database.js
+++ back/config/database.js
@@ -3,5 +3,5 @@ const mongoHost = process.env.MONGO_HOST;
 module.exports = {
     dbName: 'barnacle',
     dumpFileName: 'barnacle.test.gz',
-    url: `mongodb://${mongoHost}/barnacle`
+    url: `mongodb://barnacle:barnacle@${mongoHost}/barnacle`
 };
